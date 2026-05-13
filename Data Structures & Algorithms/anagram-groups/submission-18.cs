e18d0555efb3bcafa8b1df9a5977938b846bb857
Nguyễn Thành Nam <157128585+manaxpow@users.noreply.github.com>
2026-05-13 16:14:54 +0700

    Add: anagram-groups - submission-18

---
+++ Data Structures & Algorithms/anagram-groups/submission-18.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public List<List<string>> GroupAnagrams(string[] strs) {
+        var dict = new Dictionary<string,List<string>>();
+        for(int i=0; i< strs.Length;i++)
+        {
+            string cur = strs[i];
+            int [] charFrequent = new int [26];
+            // count the character frequency of the string
+            foreach(char c in cur)
+            {
+                charFrequent[c - 'a'] ++;
+            }
+
+            string key = String.Join('#',charFrequent);
+            // add if exist
+            if(!dict.ContainsKey(key))
+            {
+                dict[key] = new List<string>();
+            }
+            dict[key].Add(strs[i]);
+        }
+        return dict.Values.ToList<List<string>>();
+    }
+}
