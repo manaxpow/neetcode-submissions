b95655db2acca83b90aac477a2fc00d5b0e6bd3e
Nguyễn Thành Nam <157128585+manaxpow@users.noreply.github.com>
2026-05-30 15:08:54 +0700

    Add: trapping-rain-water - submission-6

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-6.cs
@@ -0,0 +1,50 @@
+public class Solution {
+    public int Trap(int[] height) {
+        int [] prefix = new int [height.Length];
+        int [] suffix = new int [height.Length];
+
+        int i =0;
+        int max =0;
+        // calculate the prefix max of index
+        while(i < height.Length)
+        {
+            if(i==0)
+            {
+                prefix[i]=0;
+            }
+            else
+            {
+                max = Math.Max(max, height[i-1]);
+                prefix[i] = max;
+            }
+            i++;
+        }
+        i= height.Length-1;
+        max =0;
+        // calculate the suffix max of index
+        while(i > 0)
+        {
+            if(i== height.Length-1)
+            {
+                suffix[i]=0;
+            }
+            else
+            {
+                max = Math.Max(max, height[i+1]);
+                suffix[i] = max;
+            }
+            // keep the min value
+            prefix[i] = Math.Min(prefix[i], suffix[i]);
+            i--;
+        }
+
+        int result =0;
+        // trapped water = min(prefix,suffix) - height
+        for(int j=0; j< prefix.Length; j++)
+        {
+            if(prefix[j] - height[j] >= 0) result += prefix[j] - height[j];
+        }
+
+        return result;
+    }
+}
