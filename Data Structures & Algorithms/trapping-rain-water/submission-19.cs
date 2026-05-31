f99d4cfdbf2a39d381f1934e423e56c98ed50479
Nguyễn Thành Nam <157128585+manaxpow@users.noreply.github.com>
2026-05-31 13:48:55 +0700

    Add: trapping-rain-water - submission-19

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-19.cs
@@ -0,0 +1,35 @@
+public class Solution {
+    public int Trap(int[] height) {
+        int left = 0;
+        int right = height.Length - 1;
+        int maxLeft = 0, maxRight = 0;
+        int result = 0;
+
+        while (left < right) {
+            if (height[left] < height[right]) {
+                if(height[left] > maxLeft)
+                {
+                    maxLeft = height[left];
+                }
+                else    
+                {
+                    result += maxLeft- height[left];
+                }
+                left++;
+            } else
+            {
+                if(height[right] > maxRight)
+                {
+                    maxRight = height[right];
+                }
+                else    
+                {
+                    result += maxRight- height[right];
+                }
+                right--;
+            }
+            
+        }
+        return result;
+    }
+}
