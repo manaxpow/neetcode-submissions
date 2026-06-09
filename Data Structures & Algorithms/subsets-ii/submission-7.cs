bfd9ffb815cc19df7e09e366308ca2c06417bf9d
Nguyễn Thành Nam <157128585+manaxpow@users.noreply.github.com>
2026-06-09 15:18:22 +0700

    Add: subsets-ii - submission-7

---
+++ Data Structures & Algorithms/subsets-ii/submission-7.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public List<List<int>> SubsetsWithDup(int[] nums) {
+        var result = new List<List<int>>();
+        Array.Sort(nums);
+        BackTracking(nums, 0, new List<int>(), result);
+        return result;
+    }
+
+    private void BackTracking(int[] nums, int cur, List<int> path, List<List<int>> result) {
+
+        result.Add(new List<int>(path));
+        for (int i = cur; i < nums.Length; i++) {
+
+            // skip the decision has the same structure with duplicate number
+            if (i > cur && nums[i] == nums[i - 1])
+                continue;
+            path.Add(nums[i]);
+            BackTracking(nums, i + 1, path, result);
+            path.RemoveAt(path.Count - 1);
+        }
+    }
+}
