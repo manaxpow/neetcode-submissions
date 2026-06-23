5a59e8387acf313998b645f7db887ede7fafe3f9
Nguyễn Thành Nam <157128585+manaxpow@users.noreply.github.com>
2026-06-23 15:37:03 +0700

    Add: duplicate-integer - submission-6

---
+++ Data Structures & Algorithms/duplicate-integer/submission-6.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public bool hasDuplicate(int[] nums) {
+        var hashSet = new HashSet<int>();
+
+        for(int i =0; i< nums.Length; i++)
+        {
+            if(hashSet.Contains(nums[i]))
+            {
+                return true; 
+            }
+            else {
+                hashSet.Add(nums[i]);
+            }
+        }
+        return false;
+    }
+}
